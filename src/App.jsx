ab19727d2f40d0a95f436464c22530902d11c57a
Nur Aleyna Pektaş <nuraleynaaaa@gmail.com>
2025-12-11 01:17:51 +0300

    main design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Nannies from './pages/Nannies';
+import Favorites from './pages/Favorites';
 
 import './App.css'
 
@@ -7,6 +10,11 @@ function App() {
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nannies" element={<Nannies />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
      
     </>
   )
---
+++ src/pages/Nannies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nannies = () => {
+  return (
+    <div>
+      Nannies
+    </div>
+  )
+}
+
+export default Nannies
